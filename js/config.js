42e289e4e48dac6b2a26855823a3ead2467ae8ff
tomtops43-png <tomtops43@gmail.com>
2026-07-21 13:30:03 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 /* Single place to configure the backend. Paste your GAS Web App /exec URL here. */
 window.APP_CONFIG = {
   // e.g. 'https://script.google.com/macros/s/AKfycb.../exec'
-  GAS_URL: 'PASTE_YOUR_GAS_WEB_APP_URL_HERE',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbxuf_aeKihJ49fzoowNBUOU7zAFiMXVtxwIw56v_CvBrcY_NAKXRTGtB-Aa8B0IsBd0MA/exec',
   CONFIG_CACHE_MINUTES: 10,
   FETCH_RETRIES: 2
 };
